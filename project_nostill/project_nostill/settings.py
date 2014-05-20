c22c53b15cba77e2e9718a5c20313d618da7733a
Engin Arslan <hibernationtheory@gmail.com>
2014-05-19 20:46:39 -0400

    adds the database related info to settings.py

--- project_nostill/project_nostill/settings.py
+++ project_nostill/project_nostill/settings.py
@@ -6,6 +6,7 @@ SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_DIR = os.path.abspath(os.path.join(SETTINGS_DIR, os.pardir))
 STATIC_DIR = os.path.join(PROJECT_DIR, "static")
 TEMPLATE_DIR = os.path.join(PROJECT_DIR, "templates")
+DATABASE_PATH = os.path.join(PROJECT_DIR, "nostill.db")
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -18,8 +19,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
